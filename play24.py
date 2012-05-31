74654068c3533361214df8f1ef194dd507d36995
Jason Marziani <jmarziani@Jason-Marzianis-MacBook-Pro-2.local>
2012-05-30 19:05:44 -0700

    fixed passing arguments in command line reading as str.  updated combinations to allow for all possible combinations of the provided args

--- play24.py
+++ play24.py
@@ -46,35 +46,41 @@ def testPatterns(args):
                 '%i*(%i-%i)+%i',
                 '%i*(%i+%i)-%i']
     for pattern in patterns:
-        yield pattern % (args[0], args[1], args[2], args[3])
+        yield pattern % (int(args[0]), int(args[1]), int(args[2]), int(args[3]))
 
+# GENERATE ALL POSSIBLE COMBINATIONS FROM ARGS PROVIDED
+# Code based on the following documetion
+# http://docs.python.org/library/itertools.html#itertools.combinations
+def combinations(iterable, r):
+    pool = tuple(iterable)
+    n = len(pool)
+    for indices in itertools.permutations(range(n), r):
+        yield tuple(pool[i] for i in indices)
 
 # GAMEPLAY
 def play24(ints):
-    
     target = 24
     for int in ints:
         print int
     print "thinking..."
 
-    for combo in itertools.product(ints, repeat=4):
+    for combo in combinations(ints, 4):
         print "COMBO:", combo
         for test in testPatterns(combo):
             print test
             if eval(test) == target:
                 print "FOUND: ", test
                 return
-        print "no patterns result in %i" % target
+    print "no patterns result in %i" % target
     
 # Define a main() function that prints a little greeting.
 def main():
     ints = sys.argv[1:]
     if not ints:
         print "usage: [--int 1-10][--int 1-10][--int 1-10][--int 1-10]";
-        #ints = [10, 10, 2, 4]
-        ints = [10,8,4,2]
-        #sys.exit(1)
-    
+        # SET A DEFAULT PATTERN TO PLAY AGAINST
+        ints = [4,10,4,2]
+       
     for i in ints:
         if(i == 0): 
             print "no 0's please"
@@ -83,8 +89,6 @@ def main():
     if len(ints) > 4:
         print "only 4 arguments, please";
         sys.exit(1)
-        #ints = [10,8,4,2]
-        #ints = [10, 10, 2, 4]
         
     play24(ints)
     print "end"
